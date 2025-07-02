cd9fd54b733333e9af23260189b655801e31cd87
muhaimin365 <ab.muhaimin95@gmail.com>
2025-07-01 21:37:28 -0700

    first commit

---
+++ V-1.py
@@ -0,0 +1,7 @@
+#Creat vitual environment
+
+PS C:\Muhaimin\Virtual-Environment> py -3.12 -m venv .Virtual-2
+PS C:\Muhaimin\Virtual-Environment>
+PS C:\Muhaimin\Virtual-Environment> cd C:\Muhaimin\Virtual-Environment\.Virtual-2
+PS C:\Muhaimin\Virtual-Environment\.Virtual-2> .\Scripts\activate.bat
+PS C:\Muhaimin\Virtual-Environment\.Virtual-2> pip install requests
